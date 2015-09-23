b0491e82a789ec49d4bb210b220f2e35a48baa92
MatthieuBlm <matthieu.bellamy@hotmail.fr>
2015-09-23 11:17:51 +0200

    pfiou

---
+++ tests/ItemTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+import item.Key;
+import item.Potion;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import special.Inventory;
+
+
+public class ItemTest {
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
